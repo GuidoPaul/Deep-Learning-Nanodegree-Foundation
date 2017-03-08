25745c80ecc3e2c5044c37e3b55b7dffab4cc746
GuidoPaul <Baoshenglin1994@gmail.com>
2017-03-08 22:35:58 +0800

    complete intro-to-tensorflow.

---
+++ Intro-to-TensorFlow/mnist_use_tensorflow/helper.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Filename: helper.py
+
+
+def batches(batch_size, features, labels):
+    """
+    Create batches of features and labels
+    :param batch_size: The batch size
+    :param features: List of features
+    :param labels: List of labels
+    :return: Batches of (Features, Labels)
+    """
+    assert len(features) == len(labels)
+    output_batches = []
+
+    simple_size = len(features)
+    for start_i in range(0, simple_size, batch_size):
+        end_i = start_i + batch_size
+        batch = [features[start_i:end_i], labels[start_i:end_i]]
+        output_batches.append(batch)
+
+    return output_batches
